a2ea263a532e8c22c16520e23c83558071fcd2d4
Armin Sander <armin@replicator.org>
2021-05-30 10:24:57 +0200

    Add Point to Size conversion functions to_isize() and to_size()

--- skia-safe/src/core/point.rs
+++ skia-safe/src/core/point.rs
@@ -98,6 +98,10 @@ impl IPoint {
     pub fn equals(self, x: i32, y: i32) -> bool {
         self == IPoint::new(x, y)
     }
+
+    pub fn to_isize(self) -> ISize {
+        (self.x, self.y).into()
+    }
 }
 
 pub type Vector = Point;
@@ -316,6 +320,10 @@ impl Point {
 
     pub fn dot(self, vec: Vector) -> scalar {
         Self::dot_product(self, vec)
+    }
+
+    pub fn to_size(self) -> Size {
+        (self.x, self.y).into()
     }
 }
 
