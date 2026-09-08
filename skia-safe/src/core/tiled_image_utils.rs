60222c0fab2aca4fe3beaf2c2fa18abf9b8db5a2
Armin Sander <armin@replicator.org>
2026-09-08 16:36:52 +0200

    skia-safe: document tiled image utils

--- skia-safe/src/core/tiled_image_utils.rs
+++ skia-safe/src/core/tiled_image_utils.rs
@@ -1,3 +1,10 @@
+//! `SkTiledImageUtils`' `DrawImage`/`DrawImageRect` methods are intended to be direct
+//! replacements for their [`Canvas`] equivalents. The `SkTiledImageUtils` calls will break
+//! `SkBitmap`-backed [`Image`]s into smaller tiles and draw them if the original image is too
+//! large to be uploaded to the GPU. If the original image doesn't need tiling or is already
+//! gpu-backed the `DrawImage`/`DrawImageRect` calls will fall through to the matching
+//! [`Canvas`] call.
+
 use skia_bindings::{self as sb, C_SkTiledImageUtils_DrawImageRect};
 
 use crate::{Canvas, Image, Paint, Point, Rect, SamplingOptions, canvas, prelude::*, scalar};
@@ -43,6 +50,20 @@ pub fn draw_image(
 
 pub const NUM_IMAGE_KEY_VALUES: usize = 6;
 
+/// Retrieves a set of values that can be used as part of a cache key for the provided image.
+///
+/// Unfortunately, [`Image::unique_id`] isn't sufficient as an [`Image`] cache key. In
+/// particular, `SkBitmap`-backed [`Image`]s can share a single `SkBitmap` and refer to different
+/// subsets of it. In this situation the optimal key is based on the `SkBitmap`'s generation ID
+/// and the subset rectangle.
+/// For Picture-backed images this method will attempt to generate a concise internally-based
+/// key (i.e., containing picture ID, matrix translation, width and height, etc.). For
+/// complicated Picture-backed images (i.e., those w/ a paint or a full matrix) it will fall
+/// back to using `image`'s unique key.
+///
+/// - `image` The image for which key values are desired
+///
+/// Returns the resulting key values.
 pub fn get_image_key_values(image: &Image) -> [u32; NUM_IMAGE_KEY_VALUES] {
     let mut key_values = [0u32; NUM_IMAGE_KEY_VALUES];
     unsafe { sb::C_SkTiledImageUtils_GetImageKeyValues(image.native(), key_values.as_mut_ptr()) }
